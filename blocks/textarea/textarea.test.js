36806874e41143022b6fdac664f69606d8bd0da5
tenorok <mail@tenorok.ru>
2014-06-06 17:09:10 +0400

    Тест на шаблон блока textarea

--- blocks/textarea/textarea.test.js
+++ blocks/textarea/textarea.test.js
@@ -1,3 +1,14 @@
 describe('Блок textarea.', function() {
-    
+
+    describe('Bemer.', function() {
+
+        it('Блок', function() {
+            assert.equal(
+                bemer({ block: 'textarea' }),
+                '<textarea class="textarea i-bem" data-bem="{&quot;textarea&quot;:{}}"></textarea>'
+            );
+        });
+
+    });
+
 });
