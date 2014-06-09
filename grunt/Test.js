7c34808bf48c8b87de9ac15b696b29f8920ebb7b
tenorok <mail@tenorok.ru>
2014-06-09 17:09:24 +0400

    В сборку тестов добавлен i-bem

--- grunt/Test.js
+++ grunt/Test.js
@@ -15,11 +15,14 @@ Test.prototype = {
     },
 
     _readBlockFiles: function() {
-        return ['.bemer.js', '.test.js'].reduce(function(content, postfix) {
-            return content += fs.readFileSync(
-                path.join('blocks/', this.block, this.block + postfix),
-                { encoding: 'utf8' }
-            );
+        return ['.js', '.bemer.js', '.test.js'].reduce(function(content, postfix) {
+            var filePath = path.join('blocks/', this.block, this.block + postfix);
+
+            if(fs.existsSync(filePath)) {
+                return content += fs.readFileSync(filePath, { encoding: 'utf8' });
+            } else {
+                return content;
+            }
         }.bind(this), '');
     }
 
