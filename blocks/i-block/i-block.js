7bc485c1e0b478e0a82fe0f3edb146f40cb65184
tenorok <mail@tenorok.ru>
2014-06-09 18:04:44 +0400

    Возможность получить экземпляр блока с кастомизированным bemjson

--- blocks/i-block/i-block.js
+++ blocks/i-block/i-block.js
@@ -16,10 +16,14 @@ BEM.DOM.decl('i-block', /** @lends i-block.prototype */ {}, /** @lends i-block *
     /**
      * Получить экземпляр блока.
      *
+     * @param {object} [bemjson] BEMJSON
      * @returns {BEM.DOM}
      */
-    create: function() {
-        return BEM.DOM.init($(bemer(this.getBEMJSON.apply(this, arguments)))).bem(this.getName());
+    create: function(bemjson) {
+        var bemjson = bemjson || this.getBEMJSON.apply(this, arguments),
+            block = BEM.DOM.init($(bemer(bemjson)));
+
+        return bemjson.js === false ? block : block.bem(this.getName());
     }
 
 });
--- blocks/i-block/i-block.test.js
+++ blocks/i-block/i-block.test.js
@@ -8,4 +8,10 @@ describe('i-block.', function() {
         assert.isTrue(BEM.blocks['i-block'].create() instanceof BEM.DOM);
     });
 
+    it('Получить экземпляр блока без инициализации из нестандартного BEMJSON', function() {
+        var block = BEM.blocks['i-block'].create({ block: 'i-block', js: false });
+        assert.isTrue(block instanceof jQuery);
+        assert.isFalse(block.hasClass('i-bem'));
+    });
+
 });
