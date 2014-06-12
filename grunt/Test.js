91e8d0b1486ca00cd46cbdda7415eaf67f53d2e5
tenorok <mail@tenorok.ru>
2014-06-12 20:55:32 +0400

    Сборка указанных файлов блока

--- grunt/Test.js
+++ grunt/Test.js
@@ -8,7 +8,8 @@ function Test(block) {
 Test.prototype = {
 
     build: function() {
-        var deps = '';
+        var deps = '',
+            files = [this.block];
 
         if(this.block === 'i-control') {
             deps = this._getIBlock() + this._getIComponent();
@@ -18,21 +19,27 @@ Test.prototype = {
             deps = this._getIBlock() + this._getIComponent() + this._getIControl();
         }
 
+        if(this.block === 'textarea') {
+            files = files.concat(['__control']);
+        }
+
         fs.writeFileSync(
             path.join('test', 'test.js'),
-            deps + this._readBlockFiles()
+            deps + this._readBlockFiles(files)
         );
     },
 
-    _readBlockFiles: function() {
-        return ['.js', '.bemer.js', '.test.js'].reduce(function(content, postfix) {
-            var filePath = path.join('blocks/', this.block, this.block + postfix);
+    _readBlockFiles: function(files) {
+        return files.reduce(function(content, file) {
+            return content += ['.js', '.bemer.js', '.test.js'].reduce(function(content, postfix) {
+                var filePath = path.join('blocks/', files[0], file + postfix);
 
-            if(fs.existsSync(filePath)) {
-                return content += fs.readFileSync(filePath, { encoding: 'utf8' });
-            } else {
-                return content;
-            }
+                if(fs.existsSync(filePath)) {
+                    return content += fs.readFileSync(filePath, { encoding: 'utf8' });
+                } else {
+                    return content;
+                }
+            }.bind(this), '');
         }.bind(this), '');
     },
 
